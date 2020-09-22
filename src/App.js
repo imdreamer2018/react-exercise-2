747bad29d5bfcfbeea0b6503283baccb207c4bb7
imdreamer <743295483@qq.com>
2020-09-22 22:27:43 +0800

    [yangqian] add Store Menu Products components that init project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Store from './components/Store/Store';
 
 class App extends Component {
   render() {
     return (
       <main className="app">
+        <Store />
       </main>
     );
   }
---
+++ src/components/Store/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Menu;
